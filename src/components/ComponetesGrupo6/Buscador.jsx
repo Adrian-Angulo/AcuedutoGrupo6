5ed2991305180bbd52244229d3d827bf3ef7157f
AdrianCastillo <adrianangulo1080@gmail.com>
2025-11-06 17:07:33 -0500

    agregar icono al buscador y borrar codigo anterior

--- src/components/ComponetesGrupo6/Buscador.jsx
+++ src/components/ComponetesGrupo6/Buscador.jsx
@@ -1,13 +1,26 @@
+import { Search } from "lucide-react";
 import Button from "./Button";
 
-const Buscador = ({placeholder }) => {
-    return (    
+const Buscador = ({ placeholder, onClick }) => {
+    return (
+        <div 
+            className="flex items-center w-1/3 h-12 ml-auto px-4 text-sm text-gray-700 border border-blue-300 rounded-lg shadow-sm placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition"
+        >
+            <button
+                onClick={onClick}
+                className="flex items-center justify-center"
+                >
+                <Search className="w-5 h-12 text-blue-600 hover:cursor-pointer" />
             
-                 <input
-                    type="text"
-                    placeholder={placeholder}
-                    className="w-1/3 h-12 ml-auto px-4 text-sm text-gray-700 border border-blue-300 rounded-lg shadow-sm placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition"
-                />
+            </button>
+            <input
+                type="text"
+                placeholder={placeholder}
+                className=" h-12 ml-auto px-4 text-sm text-gray-700 w-full border-none  placeholder:text-gray-400 focus:outline-none focus:ring-0"
+            />
+
+        </div>
+
     );
 
 }
--- src/pages/MatriculasPage.jsx
+++ src/pages/MatriculasPage.jsx
@@ -1,53 +1,13 @@
 import Buscador from '../components/ComponetesGrupo6/Buscador'
-
-
-
-
 import HeaderPage from '../components/HeaderPage';
 import Card from '../components/Card';
 import Select from '../components/Select';
 import Table from '../components/Table';
-import 'preline/preline'
+
 
 
 
 export default function MatriculasPage() {
-  // TODO: Cargar matrículas desde la API usando getMatriculas()
-  // TODO: Agregar formulario para crear nuevas matrículas
-  // TODO: Implementar funciones de editar y eliminar
-
-  /*  const [lista, setLista] = useState([]);
-   const [busqueda, setBusqueda] = useState('');
-   const [cargando, setCargando] = useState(true);
-   const navigate = useNavigate();
- 
-  */
-
-  /*   useEffect(() => {
-      const cargar = async () => {
-        try {
-          setCargando(true)
-          const datos = await getMatriculas();
-          setLista(datos || []);
-        } catch (error) {
-          console.log('Error al cargar matricuas: ', error);
-        } finally {
-          setCargando(false)
-        }
-      };
-      cargar();
-    }, []); */
-
-  // Filtrar la lista basándose en el término de búsqueda
-  /*  const listaFiltrada = lista.filter((m) => {
-     const termino = busqueda.toLowerCase().trim();
-     if (!termino) return lista; // Si no hay búsqueda, mostrar todo
- 
-     return (
-       m.cod_matricula.toLowerCase().includes(termino)
-     );
-   }); */
-
 
 
 
